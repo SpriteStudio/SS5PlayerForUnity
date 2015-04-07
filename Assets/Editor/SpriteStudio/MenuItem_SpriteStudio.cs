0be6438c09d680c4763cc642af94b31ee85ef0ce
DKAlpha <dkredint@gmail.com>
2015-04-07 15:43:08 +0900

    バージョン番号を1.2.3に変更

--- Assets/Editor/SpriteStudio/MenuItem_SpriteStudio.cs
+++ Assets/Editor/SpriteStudio/MenuItem_SpriteStudio.cs
@@ -117,7 +117,7 @@ public sealed class MenuItem_SpriteStudio : EditorWindow
 	[MenuItem("Tools/SpriteStudio/About")]
 	static void About()
 	{
-		string VersionText = "1.2.3 (RC)";
+		string VersionText = "1.2.3";
 		EditorUtility.DisplayDialog(	"SpriteStudio 5 Player for Unity",
 										"Version: " + VersionText
 										+ "\n\n"
