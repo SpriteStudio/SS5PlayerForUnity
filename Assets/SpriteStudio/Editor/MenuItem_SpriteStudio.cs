6cc8c9cc6575f30e7267ccbcd3bc81d14c6cac78
MasamiYitsuse <yitsuse@din.or.jp>
2016-04-15 03:01:24 +0900

    Ver.1.3.10 ・エフェクトのパラメータで、Z回転速度変更の到達時間を0（もしくは近似する相当小さな値）を設定した場合に0除算が起こるバグを修正

--- Assets/SpriteStudio/Editor/MenuItem_SpriteStudio.cs
+++ Assets/SpriteStudio/Editor/MenuItem_SpriteStudio.cs
@@ -462,7 +462,7 @@ public sealed class MenuItem_SpriteStudio : EditorWindow
 	[MenuItem("Tools/SpriteStudio/About")]
 	static void About()
 	{
-		string VersionText = "1.3.9";
+		string VersionText = "1.3.10";
 		EditorUtility.DisplayDialog(	"SpriteStudio 5 Player for Unity",
 										"Version: " + VersionText
 										+ "\n\n"
