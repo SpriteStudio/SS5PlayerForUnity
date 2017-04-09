496b2ce35e997a9880cab1fc890083d21ea24251
MasamiYitsuse <yitsuse@din.or.jp>
2017-04-10 02:28:55 +0900

    Ver.1.5.3 ・エフェクトの再生でMesh.trianglesのsetが余分に呼ばれている箇所を最適化（omachi様からのpull-request: 2017/04/07） ・Calculate In Advanceオプション時のMesh変更処理の最適化（omachi様からのpull-request: 2017/04/07）

--- Assets/SpriteStudio/Editor/MenuItem_SpriteStudio.cs
+++ Assets/SpriteStudio/Editor/MenuItem_SpriteStudio.cs
@@ -482,7 +482,7 @@ public sealed class MenuItem_SpriteStudio : EditorWindow
 	[MenuItem("Tools/SpriteStudio/About")]
 	static void About()
 	{
-		string VersionText = "1.5.1";
+		string VersionText = "1.5.3";
 		EditorUtility.DisplayDialog(	"SpriteStudio 5 Player for Unity",
 										"Version: " + VersionText
 										+ "\n\n"
