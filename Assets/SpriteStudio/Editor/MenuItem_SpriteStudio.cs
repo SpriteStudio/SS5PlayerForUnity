691dfb69feebc131675afb0e4c551fe783446bd0
MasamiYitsuse <yitsuse@din.or.jp>
2017-04-24 00:28:20 +0900

    Ver.1.5.7 （pull-request: omachi様 2017/04/14 Marge） ・描画マテリアルが複数存在する際のGC Allocを軽減 ・エフェクト機能描画時の初期化不全（TableIndexTriangle.Clearが欠けていた）の解消 ・エフェクト描画時のGC Allocを軽減（2件）

--- Assets/SpriteStudio/Editor/MenuItem_SpriteStudio.cs
+++ Assets/SpriteStudio/Editor/MenuItem_SpriteStudio.cs
@@ -482,7 +482,7 @@ public sealed class MenuItem_SpriteStudio : EditorWindow
 	[MenuItem("Tools/SpriteStudio/About")]
 	static void About()
 	{
-		string VersionText = "1.5.3";
+		string VersionText = "1.5.7";
 		EditorUtility.DisplayDialog(	"SpriteStudio 5 Player for Unity",
 										"Version: " + VersionText
 										+ "\n\n"
