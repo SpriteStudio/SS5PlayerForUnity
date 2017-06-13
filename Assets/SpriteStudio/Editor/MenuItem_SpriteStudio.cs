9b103ea7d3a1ee545c58b80c61a556fee91fca35
MasamiYitsuse <yitsuse@din.or.jp>
2017-06-14 03:45:25 +0900

    Ver.1.5.14 ・SSAEの構成とInstantiate時のGameObjectの生成順によって、「エフェクト」アニメーションの初期化不全が起こる場合があるバグを修正（同時に「インスタンス」アニメーションの初期化処理も若干強化）

--- Assets/SpriteStudio/Editor/MenuItem_SpriteStudio.cs
+++ Assets/SpriteStudio/Editor/MenuItem_SpriteStudio.cs
@@ -482,7 +482,7 @@ public sealed class MenuItem_SpriteStudio : EditorWindow
 	[MenuItem("Tools/SpriteStudio/About")]
 	static void About()
 	{
-		string VersionText = "1.5.13";
+		string VersionText = "1.5.14";
 		EditorUtility.DisplayDialog(	"SpriteStudio 5 Player for Unity",
 										"Version: " + VersionText
 										+ "\n\n"
