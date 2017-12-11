e6eb13ad0154f8762664fec6c0c16d78ddd1b1f1
MasamiYitsuse <yitsuse@din.or.jp>
2017-12-11 16:21:57 +0900

    Ver.1.6.0 （Unity5以降専用。開発終了版・メンテナンスフェイズに入ります） ・メッシュの合成にCombineMeshesを使用しない（omachi様からのプルリクエスト 2017/11/17 9:42 を反映） ※ただし、本改造でUnity4以前にはない関数・仕様を使用しているため、Unity5系のみへの対応になっています。

--- Assets/SpriteStudio/Editor/MenuItem_SpriteStudio.cs
+++ Assets/SpriteStudio/Editor/MenuItem_SpriteStudio.cs
@@ -482,7 +482,7 @@ public sealed class MenuItem_SpriteStudio : EditorWindow
 	[MenuItem("Tools/SpriteStudio/About")]
 	static void About()
 	{
-		string VersionText = "1.5.17";
+		string VersionText = "1.6.0";
 		EditorUtility.DisplayDialog(	"SpriteStudio 5 Player for Unity",
 										"Version: " + VersionText
 										+ "\n\n"
